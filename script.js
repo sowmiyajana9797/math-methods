44583b280ef900688a5a1c20d2def1a29b4915c8
sowmiyajana9797 <sowmiya.janar@gmail.com>
2024-09-26 12:42:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// Math Methods
+
+// 1. Basic Arithmetic:
+let x = 40;
+let y = 7;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+// output
+// 47
+// 33
+// 280
+// 5.714285714285714
+// 5
+
+// Math Object
+
+// 1.Math.round:
+console.log(Math.round(75.4));
+
+// output
+// 75
+
+// 2.Math.ceil:
+console.log(Math.ceil(5.2));
+
+// output
+// 6
+
+// 3.Math.floor:
+console.log(Math.floor(6.7));
+
+// output
+// 6
+
+// 4.Math.random:
+console.log(Math.random()*12);
+
+// output
+// 5.637835764588513
+
+// 5.Math.sqrt:
+console.log(Math.sqrt(81));
+
+// output
+// 9
+
+// Number Conversion:
+
+// 1.parseInt:
+let number = "2000";
+console.log(parseInt(number));
+
+// output
+// 2000
+
+// 2.parseFloat:
+let  float = "89.7";
+console.log(parseFloat(float));
+
+// output
+// 89.7
